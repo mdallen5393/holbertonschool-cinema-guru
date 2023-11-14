6faa4ca3e0beecab5e6f43e748f6b3c3c4f326f4
mdallen5393 <allen_matthew@outlook.com>
2023-11-13 18:54:13 -0600

    Added routing to Dashboard component

--- cinema-guru/src/routes/dashboard/Dashboard.js
+++ cinema-guru/src/routes/dashboard/Dashboard.js
@@ -2,15 +2,25 @@ import './dashboard.css';
 import Header from '../../components/navigation/Header';
 import SideBar from '../../components/navigation/Sidebar';
 
+import { BrowserRouter, Routes, Route, Navigate } from react-router-dom;
+
 const Dashboard = ({userUsername, setIsLoggedIn}) => {
     return (
+        <BrowserRouter>
         <div className='dashboard-div'>
             <Header
                 userUsername={userUsername}
                 setIsLoggedIn={setIsLoggedIn}
             />
             <SideBar />
+            <Routes>
+                <Route path='/home' element={<HomePage />} />
+                <Route path='/favorites' element={<Favorites />} />
+                <Route path='/watchlater' element={<WatchLater />} />
+                <Route path='*' element={<Nagivate to='/home' />} />
+            </Routes>
         </div>
+        </BrowserRouter>
     )
 }
 
